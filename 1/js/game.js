7e5f0a9169b8395c48111b55f4739f3ef24518eb
Mathieu de Ruiter <mathieu@fellicht.nl>
2015-07-13 16:28:39 +0200

    Step 2: Basic class & animation

--- 1/js/game.js
+++ 1/js/game.js
@@ -2,42 +2,25 @@
  * @url www.couchfriends.com
  * Tutorial: How to create a real-time multiplayer game in less than 30 minutes
  **/
-
-window.requestAnimFrame = window.requestAnimationFrame ||
-window.webkitRequestAnimationFrame ||
-window.mozRequestAnimationFrame ||
-function( callback ){
-    window.setTimeout(callback, 1000 / 60);
-};
-
 var game = {
         width: window.innerWidth,
         height: window.innerWidth
     },
     elCanvas,
-    ctx,
-    GameObjects = [];
-
+    ctx;
 window.onload = init;
 window.onresize = resizeGame;
-
-function render(time) {
-
-    ctx.clearRect(0, 0, game.width, game.height);
-    requestAnimFrame(render);
-    for (var i = 0; i < GameObjects.length; i++) {
-        GameObjects[i].update(time);
-        GameObjects[i].draw();
-    }
-}
 
 function init() {
     elCanvas = document.getElementById('game');
     ctx = elCanvas.getContext('2d');
     resizeGame();
-    var obstacle = new Obstacle();
-    obstacle.add();
-    requestAnimFrame(render);
+
+    // Draw the player
+    ctx.beginPath();
+    ctx.rect(game.width/2, game.height-60, 50, 50);
+    ctx.fillStyle = "red";
+    ctx.fill();
 }
 
 /**
